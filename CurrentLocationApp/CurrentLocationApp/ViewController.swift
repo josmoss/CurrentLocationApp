c7edda89170cbcc39ebe7b1605dea0663fd16090
josmoss <josmoss@gmail.com>
2016-07-11 15:51:45 -0600

    Set up the MapView.

---
+++ CurrentLocationApp/CurrentLocationApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CurrentLocationApp
+//
+//  Created by Joe Moss on 7/11/16.
+//  Copyright © 2016 Iron Yard_Joe Moss. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
